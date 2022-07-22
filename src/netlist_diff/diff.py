fb4d764d459836b83ce8cf5b74ff929c56e455e5
Romilly Cocking <romilly.cocking@gmail.com>
2022-07-22 15:41:01 +0100

    Refactoring

--- src/netlist_diff/diff.py
+++ src/netlist_diff/diff.py
@@ -1,8 +1,22 @@
 import sys
 
 
+def read(source):
+    with open(source) as sf:
+        return sf.read()
+
+
+def diff_text(source, source_content, target, target_content):
+    if source_content == target_content:
+        return('No difference between %s and %s' % (source, target))
+    else:
+        return('%s and %s differ' % (source, target_content))
+
+
 def diff(source, target):
-    print('No difference')
+    source_content = read(source)
+    target_content = read(target)
+    print(diff_text(source, source_content, target, target_content))
 
 
 if __name__ == '__main__':
--- test/netlist-diff/integration/test_diff.py
+++ test/netlist-diff/integration/test_diff.py
@@ -7,12 +7,24 @@ class DiffTestCase(unittest.TestCase):
     def test_identical_files_have_no_differences(self):
         result = subprocess.run(['python3',
                                  'src/netlist_diff/diff.py',
-                                 'test/data/file1.xml',
-                                 'test/data/file2.xml',
+                                 'test/data/555_netlist.xml',
+                                 'test/data/555_netlist.xml',
                                  ],
                                 capture_output=True)
-        self.assertEqual(result.returncode, 0)
+        if result.returncode != 0:
+            raise ValueError(result.stderr)
         assert_that(result.stdout.decode('UTF8'), contains_string('No difference'))
+
+    def test_detects_files_that_differ(self):
+        result = subprocess.run(['python3',
+                                 'src/netlist_diff/diff.py',
+                                 'test/data/555-stripboard_netlist.xml',
+                                 'test/data/555_netlist.xml',
+                                 ],
+                                capture_output=True)
+        if result.returncode != 0:
+            raise ValueError(result.stderr)
+        assert_that(result.stdout.decode('UTF8'), contains_string('differ'))
 
 
 if __name__ == '__main__':
